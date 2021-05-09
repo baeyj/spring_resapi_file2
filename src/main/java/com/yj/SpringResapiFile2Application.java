4dd216a285bb09d987370ca9ae5eb517bbe019f9
baeyj <qodbswl0529@naver.com>
2021-05-09 23:44:39 +0900

    init commit

---
+++ src/main/java/com/yj/SpringResapiFile2Application.java
@@ -0,0 +1,13 @@
+package com.yj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResapiFile2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResapiFile2Application.class, args);
+	}
+
+}
